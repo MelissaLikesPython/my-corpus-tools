853fa3becf31ea5d324840ff412ca8fe1263d5b1
MelissaLikesPython <45901415+MelissaLikesPython@users.noreply.github.com>
2021-10-04 19:03:16 +0100

    Update regexFinder.py

--- regexFinder.py
+++ regexFinder.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
-# This program searches all plain text files in a folder for a user-inputted regex.
+# This program searches all plain text files in a folder for a regex created from a user-inputted word or phrase.
 
 import os, re, sys
 
 # Prompts user to input regex.
-userRegex = re.compile(input('Enter your chosen regex:\n'), re.I)
+userRegex = re.compile(input('Enter your chosen word or phrase:\n'), re.I)
 
 # Prompts user to input file path.
 path = input('Please enter your chosen folder with file path:\n')
